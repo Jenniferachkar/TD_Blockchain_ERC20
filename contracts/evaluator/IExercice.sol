44d9cb6f1eb2cedaefed42e48aa57a5b10cb9cb5
Jennifer El Achkar <jennifer.el_achkar@edu.devinci.fr>
2025-11-17 21:59:17 +0100

    Delete contracts/evaluator directory

--- contracts/evaluator/IExercice.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
-
-interface IExercice {
-    function ex1_getStudentERC20Address() external view returns (address);
-    function ex2_getStudentSolutionAddress() external view returns (address);
-}
